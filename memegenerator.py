1469e91b68055723132ea861ba3eb19efb688f89
garand277 <145905657+garand277@users.noreply.github.com>
2024-05-30 13:49:22 +0500

    Update memegenerator.py

--- memegenerator.py
+++ memegenerator.py
@@ -5,7 +5,7 @@ import base64
 from io import BytesIO
 
 class MemeGenerator:
-    def make_unique_filename(self, source):
+    def make_unique_filename(source):
         filename, _, extension = source.rpartition('.')
         return f'{filename}_{uuid.uuid4()}.{extension}'
 
@@ -22,14 +22,11 @@ class MemeGenerator:
         image_width, image_height = img.size
         stroke_width = 5
 
-        # load font
         font = ImageFont.truetype(font=font_path, size=int(font_size))
 
-        # convert text to uppercase
         top_text = top_text.upper()
         bottom_text = bottom_text.upper()
 
-        # text wrapping
         left, top, right, bottom = font.getbbox('A')
         char_width = right - left
         char_height = bottom - top
@@ -37,7 +34,6 @@ class MemeGenerator:
         top_lines = textwrap.wrap(top_text, width=chars_per_line)
         bottom_lines = textwrap.wrap(bottom_text, width=chars_per_line)
 
-        # draw top lines
         y = 0
         for line in top_lines:
             left, top, right, bottom = font.getbbox(line)
@@ -47,7 +43,6 @@ class MemeGenerator:
             draw.text((x, y), line, fill='white', font=font, stroke_width=stroke_width, stroke_fill='black')
             y += line_height + 7
 
-        # draw bottom lines
         y = image_height - char_height * len(bottom_lines) - 25
         for line in bottom_lines:
             left, top, right, bottom = font.getbbox(line)
